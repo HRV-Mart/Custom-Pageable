27ba489cd74cfa43abfcea615f4e51c0be5464ac
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-17 12:50:14 +0530

    First commit

---
+++ src/main/kotlin/com/hrv/mart/custompageable/CustomPageableApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.custompageable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomPageableApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomPageableApplication>(*args)
+}
---
+++ src/test/kotlin/com/hrv/mart/custompageable/CustomPageableApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.custompageable
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomPageableApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
